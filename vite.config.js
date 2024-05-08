c31f66b2e5894fac2ff4b1f12124d2f9d6aa3801
SitoDP <jesusdprogram@gmail.com>
2024-05-08 14:22:59 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://sitodp.github.io/Rick-and-Morty/",
+
   plugins: [react()],
+  base: "/Rick-and-Morty/",
 })
